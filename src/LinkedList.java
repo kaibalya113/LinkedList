ad43a13dad8c5c2e5994b3b7a71fba4c0ccaf159
kaibalya123 <kaibalyamohanty0@gmail.com>
2020-10-14 22:26:57 +0530

    reverse a linkedlist

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -167,6 +167,76 @@ class Linked{
 		}
 		temp.next = in;
 	}
+	//Rotate the Linked List in Anticlock-wise by k nodes
+	void rotateAntiClockWise(int k) {
+		if (k == 0) 
+            return; 
+  
+        // Let us understand the below code for example k = 4 
+        // and list = 10->20->30->40->50->60. 
+        Node current = head; 
+  
+        // current will either point to kth or NULL after this 
+        // loop. current will point to node 40 in the above example 
+        int count = 1; 
+        while (count < k && current != null) { 
+            current = current.next; 
+            count++; 
+        } 
+  
+        // If current is NULL, k is greater than or equal to count 
+        // of nodes in linked list. Don't change the list in this case 
+        if (current == null) 
+            return; 
+  
+        // current points to kth node. Store it in a variable. 
+        // kthNode points to node 40 in the above example 
+        Node kthNode = current; 
+  
+        // current will point to last node after this loop 
+        // current will point to node 60 in the above example 
+        while (current.next != null) 
+            current = current.next; 
+  
+        // Change next of last node to previous head 
+        // Next of 60 is now changed to node 10 
+  
+        current.next = head; 
+  
+        // Change head to (k+1)th node 
+        // head is now changed to node 50 
+        head = kthNode.next; 
+  
+        // change next of kth node to null 
+        kthNode.next = null; 
+		
+		
+	}
+	
+	// Reverse linkedlist
+	void reverseLinkedList() {
+		Node node = head;
+		Node prev = null;
+		Node next = null;
+	  /*1
+		9
+		2
+		12
+		13
+		14
+		15
+		16
+		17
+		100*/
+		while(node != null) {
+			next = node.next;
+			node.next = prev;
+			prev = node;
+			node = next;
+		}
+		head = prev;
+	}
+	
 }
 public class LinkedList {
 	public static void main(String[] args) {
@@ -181,13 +251,16 @@ public class LinkedList {
 		list.insertFront(1);
 		list.insertEnd(100);
 		list.insertAnyPoint(9, 2);
-		list.insertAnyPoint(90, 3);
+		//list.insertAnyPoint(90, 3);
 		//list.deleteLastNode();
 //		list.deleteFirstNode();
 //		list.deleteNodeAtPosition(8);
 		System.out.println("size of linkedlist: "+list.getSize());
 		System.out.println("Is data found: "+list.searchNode(16));
-		list.rotateLinkedList(3);
+		//list.rotateLinkedList(3);
+		//list.rotateAntiClockWise(2);
+		//list.reverseLinkedList();
+		list.reverseLinkedList();
 		list.show();
 	}
 }
